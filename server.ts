88a7c2e995ed4576b7a2b8276358b6cad8d7059a
mae masters <maemastersdev@gmail.com>
2022-09-10 22:15:07 +0100

    Added get, post and delete route params for pastes.

--- server.ts
+++ server.ts
@@ -28,15 +28,65 @@ client.connect();
 
 // Here, creating route parameters for the paste functionality of the app.
 
+// Route Param to POST a paste 
+
+app.post("/pastes/addAPaste", async (req,res) => {
+  try {
+    const {pastebody, pastetitle} = req.body;
+    if (!pastebody){
+    res.send('Please add a paste')
+    }
+    else {
+      const newPaste = await client.query('INSERT INTO pastes (pastebody, title) VALUES ($1, $2) RETURNING *', [pastebody, pastetitle])
+      res.json(newPaste.rows);
+    }
+  } catch(err) {
+    res.status(500).send("There has been an error, see console for details.");
+    console.error(err);
+  }
+})
+
 // Route Param to get all pastes
 app.get("/pastes", async (req, res) => {
   try {
     const allPastes = await client.query('SELECT * FROM pastes ORDER BY DATE DESC')
     res.json(allPastes.rows);
   } catch(err) {
-    console.error(err.message);
+    res.status(500).send("There has been an error, see console for details.");
+    console.error(err);
   }
 });
+
+// Route Param to get a specific paste
+// Here pasteid is currently a string (hopefully that is fine?)
+app.get("/pastes/:pasteid", async (req, res) => {
+  try {
+    const pasteid = req.params.pasteid;
+    const specificPaste = await client.query('SELECT * FROM pastes WHERE pasteid=$1',[pasteid]);
+    res.json(specificPaste.rows);
+  } catch(err) {
+    res.status(500).send("There has been an error, see console for details.");
+    console.error(err);
+  }
+});
+
+
+// Route Param to delete a specific paste
+// Here pasteid is currently a string (hopefully that is fine?)
+app.delete("/pastes/:pasteid", async (req, res)=>{
+  try{
+    const pasteid  = req.params.pasteid;
+    const deletePost= await client.query('DELETE FROM paste_entries WHERE pasteid=$1',[pasteid])
+    res.send(`post with id: ${pasteid} has been deleted`)
+  }
+  catch(err){
+    res.status(500).send("There has been an error, see console for details.");
+    console.error(err);
+  }
+})
+
+// Route Param to delete all pastes
+// To add later - perhaps a danger zone feature.
 
 // Creating route parameters for the comments functionality of the app.
 
